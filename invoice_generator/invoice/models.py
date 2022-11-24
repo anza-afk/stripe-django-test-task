b3abfc7cdd6eaa57de671681071b81e6f340f9e4
anza-afk <zapokami@gmail.com>
2022-11-24 12:11:25 +0300

    simple minimum

--- invoice_generator/invoice/models.py
+++ invoice_generator/invoice/models.py
@@ -5,3 +5,6 @@ class Item(models.Model):
     name = models.CharField(max_length = 100)
     description = models.TextField(max_length = 500)
     price = models.CharField(max_length = 100)
+
+    def __str__(self):
+        return f'(id:{self.id}) {self.name}'
--- invoice_generator/invoice/views.py
+++ invoice_generator/invoice/views.py
@@ -3,18 +3,20 @@ from django.http import HttpResponse, HttpRequest, JsonResponse
 from .models import Item
 import stripe
 from django.conf import settings
+import os
 
 stripe.api_key = settings.STRIPE_SECRET_KEY
-PUBLIC_KEY = settings.STRIPE_PUBLIC_KEY
 
+domain_url = 'http://127.0.0.1:8000/item/'
 def get_session_id(request: HttpRequest, id: int) -> JsonResponse:
     """API endpoind returning stripe session id"""
     item = Item.objects.get(id=id)
     item_price = item.price
-    quantity=request.GET.get('quantity', 1)
+    quantity = request.GET.get('quantity', 1)
+
     checkout_session = stripe.checkout.Session.create(
-        cancel_url='https://example.com/cancel',
-        success_url='https://example.com/success',
+        cancel_url=f'{domain_url}{item.id}?payment_status=cancel',
+        success_url=f'{domain_url}{item.id}?payment_status=success',
         mode = 'payment',
         line_items=[
             {
@@ -26,12 +28,14 @@ def get_session_id(request: HttpRequest, id: int) -> JsonResponse:
     return JsonResponse({'session_id': checkout_session.id})
 
 
-
-
 def get_invoice(request: HttpRequest, id: int):
     item = Item.objects.get(id=id)
+    payment_status = request.GET.get('payment_status')
+    quantity = request.GET.get('quantity', 1)
     context = {
-        'stripe_pk': PUBLIC_KEY,
-        'item': item
+        'stripe_pk': settings.STRIPE_PUBLIC_KEY,
+        'item': item,
+        'payment_status': payment_status,
+        'quantity': quantity,
     }
     return render(request, 'index.html', context=context)
