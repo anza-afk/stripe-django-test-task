01eabdf704f3f91060929f065c03fac449ab66e6
anza-afk <zapokami@gmail.com>
2022-11-24 13:16:03 +0300

    class based views

--- invoice_generator/invoice/urls.py
+++ invoice_generator/invoice/urls.py
@@ -1,23 +1,7 @@
-"""invoice_generator URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('buy/<int:id>/', views.get_session_id, name='session'),
-    path('item/<int:id>/', views.get_invoice, name='invoice'),    
+    path('item/<int:id>',views.InvoiceView.as_view(template_name='index.html'), name='invoice'),  
+    path('buy/<int:id>', views.SessionView.as_view(), name='session'),
 ]
